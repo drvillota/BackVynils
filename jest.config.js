4b2b3edc684d734bba72e586051f88a4d88d5354
daaruiz_cib <daaruiz@bancolombia.com.co>
2024-11-08 20:32:18 -0500

    update sonar coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-    collectCoverage: true,
-    coverageDirectory: "coverage-reports", // Directorio deseado
-    roots: ["./src"], // Asegúrate de que este sea el directorio raíz de tu código
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage-reports',
+  roots: ['./src'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+};
