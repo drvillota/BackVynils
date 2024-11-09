18566a06be4efd27269b691b9bcba2e5fb065258
daaruiz_cib <daaruiz@bancolombia.com.co>
2024-11-08 20:15:45 -0500

    config coverage sonar

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    collectCoverage: true,
+    coverageDirectory: "coverage-reports", // Directorio deseado
+    roots: ["./src"], // Asegúrate de que este sea el directorio raíz de tu código
+  };
+  
